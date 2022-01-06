17aaae08e38015afc9c1f773f67afd1fef3a4439
Natarajan <nataraj.itsolution@gmail.com>
2022-01-06 16:19:36 +0530

    CURD Operations

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddnewComponent } from './addnew/addnew.component';
+import { CustomerComponent } from './customer/customer.component';
+import { HomeComponent } from './home/home.component';
+import { ListingComponent } from './listing/listing.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: HomeComponent },
+  {
+    path: "customer", component: CustomerComponent,
+    children: [{
+      path: "", component: ListingComponent
+    },
+    { path: "create", component: AddnewComponent },
+    { path: "Edit/:id", component: AddnewComponent }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { CustomerComponent } from './customer/customer.component';
+import { ListingComponent } from './listing/listing.component';
+import { AddnewComponent } from './addnew/addnew.component';
+import {HttpClientModule} from '@angular/common/http';
+import {ReactiveFormsModule} from '@angular/forms';
+import {CookieService} from 'ngx-cookie-service'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CustomerComponent,
+    ListingComponent,
+    AddnewComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,HttpClientModule,ReactiveFormsModule
   ],
-  providers: [],
+   providers: [CookieService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
